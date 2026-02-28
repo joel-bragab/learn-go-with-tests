277a2281b0b8389027f57282537a7a5824d9f0c3
Joel <joel.bragab@yahoo.com>
2026-02-28 11:09:10 -0300

    Add iteration test

---
+++ iteration/iteration_test.go
@@ -0,0 +1,25 @@
+package iteration
+
+import (
+	"testing"
+)
+
+const repeatCount = 5
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a")
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func Repeat(character string) string {
+	var repeated string
+	// alternative for range repeatCount
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
+	}
+	return repeated
+}
